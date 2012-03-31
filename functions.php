7ffb9697935289fbe61ae52b7905b38e79b6d07f
Tammy Hart <tammy@tammyhartdesigns.com>
2012-03-31 18:32:21 -0500

    functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+include_once(TEMPLATEPATH.'/functions/towns.php');
+include_once(TEMPLATEPATH.'/functions/meta_box.php');
+include_once(TEMPLATEPATH.'/functions/real-estate.php');
+include_once(TEMPLATEPATH.'/functions/sample.php');
+
+?>
