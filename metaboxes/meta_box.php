7c3051ab0b44901913ececacdf0b89af87a1458e
Tammy Hart <tammy@tammyhartdesigns.com>
2013-02-07 00:16:14 -0600

    added another check for post_lists to make sure $meta is an array

--- metaboxes/meta_box.php
+++ metaboxes/meta_box.php
@@ -112,7 +112,7 @@ function custom_meta_box_field( $field, $meta = null, $repeatable = null ) {
 									<option value=""></option>'; // Select One
 							$posts = get_posts( array( 'post_type' => $post_type, 'posts_per_page' => -1, 'orderby' => 'name', 'order' => 'ASC' ) );
 							foreach ( $posts as $item )
-								echo '<option value="' . $item->ID . '"' . selected( in_array( $item->ID, $meta ), true, false ) . '>' . $item->post_title . '</option>';
+								echo '<option value="' . $item->ID . '"' . selected( is_array( $meta ) && in_array( $item->ID, $meta ), true, false ) . '>' . $item->post_title . '</option>';
 							$post_type_object = get_post_type_object( $post_type );
 							echo '</select> &nbsp;<span class="description"><a href="' . admin_url( 'edit.php?post_type=' . $post_type . '">Manage ' . $post_type_object->label ) . '</a></span><br />' . $desc;
 						break;
