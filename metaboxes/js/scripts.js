c6380878da29c6256406f11a828af116f12f0f8b
Tammy Hart <tammy@tammyhartdesigns.com>
2013-02-06 23:48:22 -0600

    changed file and image upload buttons to new media uploader

--- metaboxes/js/scripts.js
+++ metaboxes/js/scripts.js
@@ -1,21 +1,53 @@
 jQuery(function($) {
 	
 	// the upload image button, saves the id and outputs a preview of the image
-	$('.meta_box_upload_image_button').click(function() {
-		formID = $(this).attr('rel');
-		formfield = $(this).siblings('.meta_box_upload_image');
-		preview = $(this).siblings('.meta_box_preview_image');
-		tb_show('Choose Image', 'media-upload.php?post_id=' + formID + '&type=image&TB_iframe=1');
-		window.orig_send_to_editor = window.send_to_editor;
-		window.send_to_editor = function(html) {
-			imgurl = html.match(/<img.*?src="(.*?)"/);
-			id = html.match(/wp-image-(.*?)"/, '');
-			formfield.val(id[1]);
-			preview.attr('src', imgurl[1]);
-			tb_remove();
-			window.send_to_editor = window.orig_send_to_editor;
+	var imageFrame;
+	$('.meta_box_upload_image_button').click(function(e) {
+		e.preventDefault();
+		
+		var options, attachment;
+		
+		$self = $(event.target);
+		$div = $self.closest('div.meta_box_image');
+		
+		// if the frame already exists, open it
+		if ( imageFrame ) {
+			imageFrame.open();
+			return;
 		}
-		return false;
+		
+		// set our settings
+		imageFrame = wp.media({
+			title: 'Choose Image',
+			multiple: false,
+			library: {
+		 		type: 'image'
+			},
+			button: {
+		  		text: 'Use This Image'
+			}
+		});
+		
+		// set up our select handler
+		imageFrame.on( 'select', function() {
+			selection = imageFrame.state().get('selection');
+			
+			if ( ! selection )
+			return;
+			
+			// loop through the selected files
+			selection.each( function( attachment ) {
+				console.log(attachment);
+				var src = attachment.attributes.sizes.full.url;
+				var id = attachment.id;
+				
+				$div.find('.meta_box_preview_image').attr('src', src);
+				$div.find('.meta_box_upload_image').val(id);
+			} );
+		});
+		
+		// open the frame
+		imageFrame.open();
 	});
 	
 	// the remove image link, removes the image id from the hidden field and replaces the image preview
@@ -27,23 +59,54 @@ jQuery(function($) {
 	});
 	
 	// the file image button, saves the id and outputs the file name
-	$('.meta_box_upload_file_button').click(function() {
-		formID = $(this).attr('rel');
-		formfield = $(this).siblings('.meta_box_upload_file');
-		preview = $(this).siblings('.meta_box_filename');
-		icon = $(this).siblings('.meta_box_file');
-		tb_show('Choose File', 'media-upload.php?post_id=' + formID + '&type=file&TB_iframe=1');
-		window.orig_send_to_editor = window.send_to_editor;
-		window.send_to_editor = function(html) {
-			fileurl = $(html).attr('href');
-			//filename = $(html).text();
-			formfield.val(fileurl);
-			preview.text(fileurl);
-			icon.addClass('checked');
-			tb_remove();
-			window.send_to_editor = window.orig_send_to_editor;
+	var fileFrame;
+	$('.meta_box_upload_file_button').click(function(e) {
+		e.preventDefault();
+		
+		var options, attachment;
+		
+		$self = $(event.target);
+		$div = $self.closest('div.meta_box_file_stuff');
+		
+		// if the frame already exists, open it
+		if ( fileFrame ) {
+			fileFrame.open();
+			return;
 		}
-		return false;
+		
+		// set our settings
+		fileFrame = wp.media({
+			title: 'Choose File',
+			multiple: false,
+			library: {
+		 		type: 'file'
+			},
+			button: {
+		  		text: 'Use This File'
+			}
+		});
+		
+		// set up our select handler
+		fileFrame.on( 'select', function() {
+			selection = fileFrame.state().get('selection');
+			
+			if ( ! selection )
+			return;
+			
+			// loop through the selected files
+			selection.each( function( attachment ) {
+				console.log(attachment);
+				var src = attachment.attributes.url;
+				var id = attachment.id;
+				
+				$div.find('.meta_box_filename').text(src);
+				$div.find('.meta_box_upload_file').val(src);
+				$div.find('.meta_box_file').addClass('checked');
+			} );
+		});
+		
+		// open the frame
+		fileFrame.open();
 	});
 	
 	// the remove image link, removes the image id from the hidden field and replaces the image preview
